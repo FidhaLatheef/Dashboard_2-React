9b919afb26f94e9c8cfa52de5860470c0e861741
unknown <fidhalatheefm@gmail.com>
2023-12-03 19:36:55 +0530

    header and profile card created

--- client/src/App.js
+++ client/src/App.js
@@ -2,7 +2,7 @@ import React from 'react';
 import Table from './components/Table';
 import PieChart from './components/PieChart';
 import Graph from './components/Graph';
-
+import ProfileCard from './components/ProfileCard';
 import SideBar from './components/sideBar';
 
 const App = () => {
@@ -10,8 +10,35 @@ const App = () => {
     <div>
       <div >
         <div className="body-overlay" />
-        <SideBar/>      
-
+        <SideBar />
+        {/* Page Content  */}
+        <div id="content">
+          <div className="top-navbar" >
+            <nav className="navbar navbar-expand-lg">
+              <div className="container-fluid">
+                <button type="button" id="sidebarCollapse" className="d-xl-block d-lg-block d-md-mone d-none">
+                  <span className="material-icons">arrow_back_ios</span>
+                </button>
+                <a className="navbar-brand" href="#"> Dashboard </a>
+                <button className="d-inline-block d-lg-none ml-auto more-button" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                  <span className="material-icons">more_vert</span>
+                </button>
+                <div className="collapse navbar-collapse d-lg-block d-xl-block d-sm-none d-md-none d-none" id="navbarSupportedContent">
+                </div>
+              </div>
+            </nav>
+          </div>
+          <div className="main-content">
+            <div className="row">
+           
+            </div>
+            <div className="row ">
+            
+           <ProfileCard/>
+            </div>
+      
+          </div>
+        </div>
       </div>
     </div>
   );
